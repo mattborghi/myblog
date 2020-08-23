9ac20bd4c02fabbe84ed61a13940a65feaecd946
Matias Borghi <borghi.matias@gmail.com>
2020-08-22 22:20:21 -0300

    changed blog theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,79 +1,92 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
 module.exports = {
-  pathPrefix: "/blog",
+  pathPrefix: '/blog', // prefix name of the blog to use when building page
   siteMetadata: {
-    title: `Matias Borghi Blog`,
-    author: `Matias Borghi`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
-    social: {
-      linkedin: `borghimatias`,
-    },
+    title: 'Blog',
+    description: 'A starter blog demonstrating what Gatsby can do.',
+    siteUrl: 'https://mattborghi.github.io/blog', // full path to blog - no ending slash
+  },
+  mapping: {
+    'MarkdownRemark.frontmatter.author': 'AuthorYaml',
   },
   plugins: [
+    'gatsby-plugin-sitemap',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-plugin-sharp',
       options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
+        quality: 100,
+        stripMetadata: true,
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
+        name: 'content',
+        path: path.join(__dirname, 'src', 'content'),
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-responsive-iframe',
             options: {
-              maxWidth: 590,
+              wrapperStyle: 'margin-bottom: 1rem',
             },
           },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+          'gatsby-remark-abbr',
           {
-            resolve: `gatsby-remark-responsive-iframe`,
+            resolve: 'gatsby-remark-images',
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
+              maxWidth: 2000,
+              quality: 100,
             },
           },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
         ],
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-json',
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        siteUrl: 'https://gatsby-casper.netlify.com',
       },
     },
-    `gatsby-plugin-feed`,
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-typescript',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-yaml',
+    'gatsby-plugin-feed',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-postcss',
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        postCssPlugins: [require('postcss-color-function'), require('cssnano')()],
       },
     },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        pathToConfigModule: `src/utils/typography`,
+        trackingId: 'UA-175899844-1',
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // IP anonymization for GDPR compliance
+        anonymize: true,
+        // Disable analytics for users with `Do Not Track` enabled
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**'],
+        // Specifies what percentage of users should be tracked
+        sampleRate: 100,
+        // Determines how often site speed tracking beacons will be sent
+        siteSpeedSampleRate: 10,
       },
     },
   ],
-}
+};
---
+++ src/content/blog/welcome-welcome/index.md
@@ -0,0 +1,16 @@
+---
+layout: post
+title: Welcome
+image: ./welcome.jpg
+author: [Matias Borghi]
+date: 2019-08-08T00:32:03.284Z
+excerpt: My first post
+tags: ['Getting Started']
+draft: false
+---
+
+This is my first post on my Gatsby Blog! How exciting!
+
+Just for fun here is a picture I took on November 2018 on [Tongariro](https://en.wikipedia.org/wiki/Tongariro_National_Park), New Zealand.
+
+![Tongariro](./tongariro.jpg)
